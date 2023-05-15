b4e70ba20145f90757ad937df903421d1ca53054
ShinReisen <shinzeroone@gmail.com>
2023-05-15 13:58:17 +0200

    work is done

--- app/main.py
+++ app/main.py
@@ -1,13 +1,27 @@
+import redis.asyncio as redisac
+from app.fastapi_limiter import FastAPILimiter
 from app.router import router
-
+from app.couriers.router import router as router_courier
+from app.orders.router import router as router_order
 from fastapi import FastAPI
+from app.config import REDIS_HOST
 
 
-def get_application() -> FastAPI:
-    application = FastAPI()
-    application.include_router(router)
+# инициализируем приложение
+app = FastAPI(title="Yandex Lavka")
 
-    return application
+# подключим основные/корневые эндпоинты
+app.include_router(router)
+
+# подключим эндпоинты по курьерам
+app.include_router(router_courier)
+
+# подключим эндпоинты по заказам
+app.include_router(router_order)
 
 
-app = get_application()
+
+@app.on_event("startup")
+async def startup():
+   redis = redisac.from_url(f"redis://{REDIS_HOST}", encoding="utf-8", decode_responses=True)
+   await FastAPILimiter.init(redis)
--- app/router.py
+++ app/router.py
@@ -1,25 +1,35 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from starlette import status
 
+from app.fastapi_limiter.depends import RateLimiter
 
-router = APIRouter()
-
-
-@router.get(
-    "/ping",
-    name='dev:ping',
-    status_code=status.HTTP_200_OK
+router = APIRouter(
+    # расскоментить для запуска лимитера
+    # dependencies=[Depends(RateLimiter(times=10, seconds=1))]
 )
-async def ping():
-    return 'pong'
 
+# Решил отключить корневые роуты для красоты докс
 
-@router.post(
-    "/hello",
-    name='dev:hello-username',
-    status_code=status.HTTP_200_OK
-)
-async def ping(request: Request):
-    request = await request.json()
-    username = request['username']
-    return f'Hello, {username}!'
+# @router.get(
+#     "/ping",
+#     name='dev:ping',
+#     status_code=status.HTTP_200_OK
+# )
+# async def ping():
+#     return 'pong'
+#
+#
+# @router.get("/")
+# def hello():
+#     return "Hello world! It's Shin!"
+#
+#
+# @router.post(
+#     "/hello",
+#     name='dev:hello-username',
+#     status_code=status.HTTP_200_OK
+# )
+# async def ping(request: Request):
+#     request = await request.json()
+#     username = request['username']
+#     return f'Hello, {username}!'
